84e8336a72f4a8377ed81993e63eb7a8bb4bf8fc
Asghar Ibraheem Baloch <46741864+asgharibraheembaloch@users.noreply.github.com>
2021-11-23 15:00:15 +0500

    update app.ts file
    added comment for the class 'Startup' method name main for learning purposes

--- step00_helloworld/app.ts
+++ step00_helloworld/app.ts
@@ -2,6 +2,7 @@
 //https://github.com/Microsoft/TypeScript/blob/master/doc/spec.md
 
 class Startup {
+    // method must be of return type nubmer
     public static main(): number {
         console.log('Hello World');
         return 0;
